5cb76e6f66e65f074650f563fbb537541e1ab5c3
gregoridelrio <gregoridelrio@gmail.com>
2025-12-30 12:52:58 +0100

    Fix query 3 add query 5 and 6

--- queries/queries.sql
+++ queries/queries.sql
@@ -5,16 +5,18 @@ SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-SELECT * FROM producto;
+SELECT codigo, nombre, precio, codigo_fabricante FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
 SELECT nombre, precio AS precio_eur, precio AS precio_usd FROM producto; 
 
 -- 5. Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (amb un tipus de canvi de 1 € = 1,1 $ i arrodonint el resultat a dues xifres decimals). Utilitza els següents àlies per a les columnes: nom del producte, euros, dòlars.
-
+SELECT nombre AS `nom del producte`, precio AS euros, ROUND(precio*1.1,2) AS `dòlars`
+FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
-
+SELECT UPPER(nombre), precio
+FROM producto;
 
 -- 7. Llista els noms (nombre) i els preus (precio) dels productes de la taula producto, convertint els noms a minúscula.
 
