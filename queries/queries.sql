796e6cbd48a60e7bb8c74b3762af5c454236ac93
gregoridelrio <gregoridelrio@gmail.com>
2025-12-30 16:13:38 +0100

    fix 33

--- queries/queries.sql
+++ queries/queries.sql
@@ -170,7 +170,7 @@ WHERE p.precio >= 180
 ORDER BY p.precio DESC, p.nombre ASC;
 
 -- 33. Retorna un llistat amb el codi i el nom de fabricant (fabricante), solament d'aquells fabricants que tenen productes associats en la base de dades.
-SELECT f.codigo, f.nombre AS fabricante
+SELECT DISTINCT f.codigo, f.nombre AS fabricante
 FROM fabricante f 
 JOIN producto p
 ON f.codigo = p.codigo_fabricante;
